c870a28615cb17680ceaf15cdbb05c66e5535bbf
Nasreen <nasreenaqmal12@gmail.com>
2026-01-21 21:00:54 +0800

    style: change navlink hover behavior

--- src/components/header/NavLink.jsx
+++ src/components/header/NavLink.jsx
@@ -3,7 +3,7 @@ export function NavLink({ mobile = false, text, tab, selectedTab, setSelectedTab
 
     const baseClass = "px-3 py-2 rounded-md text-sm font-medium cursor-pointer";
     const activeClass = "bg-zinc-100 text-black dark:bg-zinc-950 dark:text-white";
-    const inactiveClass = "text-zinc-800 hover:bg-zinc-200 hover:text-black dark:text-zinc-300 dark:hover:bg-zinc-900 dark:hover:text-white";
+    const inactiveClass = "text-zinc-800 hover:text-black dark:text-zinc-300 dark:hover:text-white";
 
     const buttonClass = `${baseClass} ${selectedTab === tab ? activeClass : inactiveClass}`;
 
