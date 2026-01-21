e457ab490711274c895fd007f4c9ad71f6f0921a
Nasreen <nasreenaqmal12@gmail.com>
2026-01-21 20:15:25 +0800

    chore: updated resume image and file

--- src/components/sections/Resume.jsx
+++ src/components/sections/Resume.jsx
@@ -1,7 +1,7 @@
 
 import resume from '../../assets/pdfs/resume.pdf'
-import page1 from '../../assets/images/resume/resume-1.png'
-import page2 from '../../assets/images/resume/resume-2.png'
+import page1 from '../../assets/images/resume/resume-1.webp'
+import page2 from '../../assets/images/resume/resume-2.webp'
 import { HiOutlineDocumentArrowDown } from "react-icons/hi2";
 
 export default function Resume() {
@@ -15,7 +15,7 @@ export default function Resume() {
                     className="inline-flex items-center gap-2 px-3 py-2 text-sm font-semibold text-white bg-green-700 rounded-md shadow-sm hover:bg-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700"
                 >
                     <HiOutlineDocumentArrowDown className='size-4 stroke-[2]' />
-                    Download
+                    Download as PDF
                 </a>
                 <img
                     src={page1}
